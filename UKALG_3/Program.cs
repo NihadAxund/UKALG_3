3c884261f37013338b70dc22ac2d428a85e9ff8d
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2023-11-01 23:51:24 +0400

    Add project files.

---
+++ UKALG_3/Program.cs
@@ -0,0 +1,431 @@
+﻿using System;
+// I LOVE OOP
+class Program
+{
+    static void Main()
+    {
+        Task1(4, 18);
+
+        //Task2(3, 21);
+
+        //Task3(16, 6);
+
+        //Task4(17, 9);
+
+        //Task5(2, 16);
+
+        //Task6(5, 19);
+
+        //Task7(20, 10);
+
+        //Task8(23, 11);
+
+        //Task9(4, 18);
+
+        //Task10(3, 21);
+
+        //Task11(16, 6);
+
+        //Task12(17, 9);
+
+        //Task13(2, 16);
+
+        //Task14(5, 19);
+
+        //Task15(20, 10);
+
+        //Task16(23, 11);
+
+        //Task17(4, 18);
+
+        //Task18(3, 21);
+
+        //Task19(16, 6);
+
+        //Task20(17, 9);
+
+        //Task21(2, 16);
+
+        //Task22(5, 19);
+
+        //Task23(20, 10);
+
+        //Task24(23, 11);
+
+        //Task25(4, 18);
+    }
+
+    static void Task1(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Even Numbers:");
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Even Numbers: " + sum);
+    }
+
+    static void Task2(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Odd Numbers:");
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Odd Numbers: " + sum);
+    }
+
+    static void Task3(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Even Numbers in Reverse Order:");
+        for (int i = start; i >= end; i--)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Even Numbers in Reverse Order: " + sum);
+    }
+
+    static void Task4(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Odd Numbers in Reverse Order:");
+        for (int i = start; i >= end; i--)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Odd Numbers in Reverse Order: " + sum);
+    }
+
+    static void Task5(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Even Numbers:");
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Even Numbers: " + sum);
+    }
+
+    static void Task6(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Odd Numbers:");
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Odd Numbers: " + sum);
+    }
+
+    static void Task7(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Even Numbers in Reverse Order:");
+        for (int i = start; i >= end; i--)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Even Numbers in Reverse Order: " + sum);
+    }
+
+    static void Task8(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Odd Numbers in Reverse Order:");
+        for (int i = start; i >= end; i--)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Odd Numbers in Reverse Order: " + sum);
+    }
+
+    static void Task9(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Even Numbers:");
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Even Numbers: " + sum);
+    }
+
+    static void Task10(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Odd Numbers:");
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Odd Numbers: " + sum);
+    }
+
+    static void Task11(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Even Numbers in Reverse Order:");
+        for (int i = start; i >= end; i--)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Even Numbers in Reverse Order: " + sum);
+    }
+
+    static void Task12(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Odd Numbers in Reverse Order:");
+        for (int i = start; i >= end; i--)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Odd Numbers in Reverse Order: " + sum);
+    }
+
+    static void Task13(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Even Numbers:");
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Even Numbers: " + sum);
+    }
+
+    static void Task14(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Odd Numbers:");
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Odd Numbers: " + sum);
+    }
+
+    static void Task15(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Even Numbers in Reverse Order:");
+        for (int i = start; i >= end; i--)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Even Numbers in Reverse Order: " + sum);
+    }
+    static void Task16(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Odd Numbers in Reverse Order:");
+        for (int i = start; i >= end; i--)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Odd Numbers in Reverse Order: " + sum);
+    }
+
+    static void Task17(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Even Numbers:");
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Even Numbers: " + sum);
+    }
+
+    static void Task18(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Odd Numbers:");
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Odd Numbers: " + sum);
+    }
+
+    static void Task19(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Even Numbers in Reverse Order:");
+        for (int i = start; i >= end; i--)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Even Numbers in Reverse Order: " + sum);
+    }
+
+    static void Task20(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Odd Numbers in Reverse Order:");
+        for (int i = start; i >= end; i--)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Odd Numbers in Reverse Order: " + sum);
+    }
+
+    static void Task21(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Even Numbers:");
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Even Numbers: " + sum);
+    }
+
+    static void Task22(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Odd Numbers:");
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Odd Numbers: " + sum);
+    }
+
+    static void Task23(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Even Numbers in Reverse Order:");
+        for (int i = start; i >= end; i--)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Even Numbers in Reverse Order: " + sum);
+    }
+
+    static void Task24(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Odd Numbers in Reverse Order:");
+        for (int i = start; i >= end; i--)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Odd Numbers in Reverse Order: " + sum);
+    }
+
+    static void Task25(int start, int end)
+    {
+        int sum = 0;
+        Console.WriteLine("Even Numbers:");
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+        }
+        Console.WriteLine("Sum of Even Numbers: " + sum);
+    }
+}
